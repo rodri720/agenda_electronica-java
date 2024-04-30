8e20f290581cafbd552fbae4b041614c21c2cf38
rodri720 <tomiyrami@gamil.com>
2024-04-29 21:08:38 -0300

    agenda_electronica

---
+++ src/agenda_electronica/Agenda_Electronica.java
@@ -0,0 +1,16 @@
+
+package agenda_electronica;
+
+import agenda_electronica.igu.Principal;
+
+
+public class Agenda_Electronica {
+
+    
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+    
+}
